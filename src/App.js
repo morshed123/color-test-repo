cbdfc63318e656103cca4cb71cfe434db78a6331
md-morshed-hossain <mdmorshedhossain38@gmail.com>
2022-11-30 21:46:26 +0600

    Color Test Repo Create Date: 30-November-2022

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ function App() {
       <div className="row g-3 d-flex justify-content-center">
         <div className="col-sm-8 color__area">
           <div className="form-group pt-3">
-            <textarea style={{ backgroundColor: `${colorName}` }} className="form-control" placeholder="Here, you will see your color area..." id="colorTextarea" rows="6"></textarea>
+            <textarea style={{ backgroundColor: `${colorName}` }} type="text" className="form-control" placeholder="Here, you will see your color area..." id="colorTextarea" rows="6"></textarea>
             <label htmlFor="colorTextarea" className="col-form-label fs-5 pt-3">Showing the Color Text-area : <i className="ms-2 fs-3 fas fa-arrow-turn-up"></i></label>
           </div>
           <div className="form-group pt-3">
             <label htmlFor="ColorCodeInput" className="col-form-label fs-5">Write Color Code Or Name : <i className="ms-2 fs-5 fas fa-arrow-turn-down"></i></label>
-            <input type="email" className="form-control" id="ColorCodeInput" placeholder="Write here color code or, name ..." onChange={(e) => setColorName(e.target.value)} />
+            <input type="text" className="form-control" id="ColorCodeInput" placeholder="Write here color code or, name ..." onChange={(e) => setColorName(e.target.value)} />
           </div>
         </div>
       </div>
