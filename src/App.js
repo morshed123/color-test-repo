d9e8090e490868df9cbacfedae662b8b3a9b2612
md-morshed-hossain <mdmorshedhossain38@gmail.com>
2022-11-30 21:39:02 +0600

    Color Test Repo Create Date: 30-November-2022

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
+
+  const [colorName, setColorName] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2 className="text-center pt-5">Color Test</h2>
+      <div className="row g-3 d-flex justify-content-center">
+        <div className="col-sm-8 color__area">
+          <div className="form-group pt-3">
+            <textarea style={{ backgroundColor: `${colorName}` }} className="form-control" placeholder="Here, you will see your color area..." id="colorTextarea" rows="6"></textarea>
+            <label htmlFor="colorTextarea" className="col-form-label fs-5 pt-3">Showing the Color Text-area : <i className="ms-2 fs-3 fas fa-arrow-turn-up"></i></label>
+          </div>
+          <div className="form-group pt-3">
+            <label htmlFor="ColorCodeInput" className="col-form-label fs-5">Write Color Code Or Name : <i className="ms-2 fs-5 fas fa-arrow-turn-down"></i></label>
+            <input type="email" className="form-control" id="ColorCodeInput" placeholder="Write here color code or, name ..." onChange={(e) => setColorName(e.target.value)} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
